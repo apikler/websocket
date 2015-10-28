1b6fc1aec476b5e615f3c35c45ad9bdde82aa6c5
apikler <apikler@gmail.com>
2015-10-28 00:43:35 -0400

    minimal draw app complete

--- draw_server.py
+++ draw_server.py
@@ -1,3 +1,5 @@
+import json
+
 from websocketserver import Server, Listener
 
 PORT = 8888
@@ -7,7 +9,19 @@ class EchoServer(Server):
         print "Connection from %s:%d" % (address, port)
     
     def onMessage(self, message, address, port):
-        print "Message from %d: %s" % (port, message)
+        try:
+            message = json.loads(message)
+            coords = message['coords']
+            x1 = float(coords[0])
+            x2 = float(coords[1])
+            if message['event'] not in ('down', 'up', 'move'):
+                raise ValueError('Invalid event %s' % message['event'])
+        except KeyError, ValueError:
+            print "Invalid message!"
+            return
+        
+        message['client'] = port
+        self.sendToOthers(json.dumps(message), port)        
     
     def onClose(self, address, port):
         print "Disconnected: %s:%d" % (address, port)
